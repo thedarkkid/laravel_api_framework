5abfb7197d28617d07365f050ac73ccd6ad678c8
Layo Folaranmi <thedarkkid.codes@gmail.com>
2022-02-03 11:30:22 +0100

    added user relation

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'body', 'user_id', 'visibility'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+
 }
